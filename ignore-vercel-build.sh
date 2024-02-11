cba9fca4b724ccb5c42488f6c1d64992d417627f
Chris <chris@cbaus.de>
2024-02-11 15:54:32 +0000

    fix branch name

--- ignore-vercel-build.sh
+++ ignore-vercel-build.sh
@@ -2,7 +2,7 @@
 
 echo "VERCEL_GIT_COMMIT_REF: $VERCEL_GIT_COMMIT_REF"
 
-if [[ $VERCEL_GIT_COMMIT_REF == "master" ]]; then
+if [[ $VERCEL_GIT_COMMIT_REF == "main" ]]; then
   echo "Build can proceed"
   exit 1;
 else
